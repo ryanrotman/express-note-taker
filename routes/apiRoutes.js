6e98c853dd262817d79d47177313378f651d7baa
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-04 22:19:15 -0500

    added API POST code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,5 +9,15 @@ module.exports = function (app) {
     });
 
     // API POST request
-    
-}
+    app.post("/api/notes", function (req, res) {
+        const allNotes = [];
+        const newNote = req.body;
+        console.log("New note: " + newNote);
+        allNotes.push(newNote);
+        let id = 0;
+        for (let i = 0; i < allNotes.length; i++) {
+            allNotes[i].id = i + 1;
+        };
+        res.json(newNote);
+    });
+};
