2d3a81cc3fa44dde60e5e845f953070bdfccb36c
Ryan Rotman <ryan.rotman@gmail.com>
2020-10-06 22:47:28 -0500

    edited POST

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,33 @@
+const { json } = require("express");
 // Dependencies
 const fs = require("fs");
+const uuid = require("uuid");
+const db = require("../db/db.json")
 
 // Routing
 module.exports = function (app) {
     // API GET request
     app.get("/api/notes", function (req, res) {
-        res.json("../db/db.json");
+        res.json(db);
     });
 
     // API POST request
     app.post("/api/notes", function (req, res) {
-        const allNotes = [];
-        const newNote = req.body;
-        console.log("New note: " + newNote);
+        let allNotes = fs.readFile(__dirname + "/../db/db.json", (err, data) => {
+            if (err) throw err;
+            JSON.parse(data);
+        });
+        const newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid.v4(),
+        }
+        console.log(newNote);
         allNotes.push(newNote);
-        let id = 0;
-        for (let i = 0; i < allNotes.length; i++) {
-            allNotes[i].id = i + 1;
-        };
         res.json(newNote);
+        fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(allNotes), (err) => {
+            if (err) throw err;
+            console.log("The note has been saved.")
+        })
     });
 };
